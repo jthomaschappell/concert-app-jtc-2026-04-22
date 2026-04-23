a76808e2eebfda4b18ee222e9e0387d7c21791bd
Your Name <youremail@example.com>
2026-04-23 05:29:47 +0000

    updated html

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ let app = new express();
 
 // set up database connection
 const knex = require("knex")({
- client: "mysql",
+ client: "mysql2",
  connection: {
-  host:"concert-db-instance-1.c61dq6ysma4i.us-east-2.rds.amazonaws.com",
+  host: "concert-db.cf8cwmywif2v.us-west-1.rds.amazonaws.com",
   user: "admin",
-  password: "Password1",
+  password: "concertdb",
   database:"paradise-concerts",
   port: 3306,
  },
@@ -18,8 +18,13 @@ app.get("/",(req,res) => {
  .select()
  .from("venues")
  .then((result) => {
-  console.log(result);
-  res.send(result);
- }); 
+ let html = "<body><ul>";
+ for (let i=0;i<result.length;i++) {
+  html += "<li>" + result[i].location + "</li>";
+ }
+  html += "</body>"
+  res.send(html);
+ });
 });
 app.listen(3000);
+
